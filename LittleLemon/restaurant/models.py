237c40bed569d65dd0e926f973402ca2b427261d
zed-chi <zed-chi@mail.ru>
2023-05-07 21:28:30 +0300

    changed models

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -8,7 +8,21 @@ class Booking(models.Model):
     booking_slot = models.SmallIntegerField(default=10)
 
 
-class Menu(models.Model):
+class MenuCategory(models.Model):
+    title = models.CharField(max_length=125)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class MenuItem(models.Model):
+    category = models.ForeignKey(
+        MenuCategory,
+        on_delete=models.SET_NULL,
+        related_name="items",
+        null=True,
+        blank=True,
+    )
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
