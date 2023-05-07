a6314f5b9fd42a6212651e3bc6c1d9d017fdf82a
zed-chi <zed-chi@mail.ru>
2023-05-07 17:26:40 +0300

    adde route

--- LittleLemon/restaurant/urls.py
+++ LittleLemon/restaurant/urls.py
@@ -1,14 +1,17 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
+
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r"tables", views.BookingViewSet)
 
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("menu/", views.MenuItemsView.as_view()),
     path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
     path("booking/", include(router.urls)),
+    path("api-token-auth/", obtain_auth_token),
 ]
