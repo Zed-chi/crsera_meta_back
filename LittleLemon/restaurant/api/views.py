a72ac81015a5e0f758d5ec99d5b9e2ed3f2eb9ce
zed-chi <zed-chi@mail.ru>
2023-05-07 21:29:14 +0300

    added viewset

--- LittleLemon/restaurant/api/views.py
+++ LittleLemon/restaurant/api/views.py
@@ -1,15 +1,16 @@
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
-from restaurant.models import Booking, Menu
+from restaurant.models import Booking, MenuCategory, MenuItem
 from restaurant.api.serializers import (
     BookingSerializer,
     MenuItemSerializer,
+    MenuCategorySerializer,
 )
 
 
 class MenuItemsView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
-    queryset = Menu.objects.all()
+    queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
 
@@ -17,11 +18,17 @@ class SingleMenuItemView(
     generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 ):
     permission_classes = [IsAuthenticated]
-    queryset = Menu.objects.all()
+    queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
 
 class BookingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class MenuCategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = MenuCategory.objects.all()
     serializer_class = BookingSerializer
