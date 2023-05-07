7e8b848107f379b0c3bb2822f472eec878a47733
zed-chi <zed-chi@mail.ru>
2023-05-07 20:01:09 +0300

    added code prom previous codebase

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,31 +1,71 @@
-from django.shortcuts import render
-from rest_framework import generics, viewsets
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-from .models import Booking, Menu
-from .serializers import BookingSerializer, MenuItemSerializer
-
-
-def index(request):
-    return render(request, "index.html", {})
+import json
+from datetime import datetime
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.views.decorators.csrf import csrf_exempt
+from restaurant.forms import BookingForm
+from restaurant.models import Booking, Menu
 
 
-# Create your views here.
-class MenuItemsView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = Menu.objects.all()
-    serializer_class = MenuItemSerializer
+def home(request):
+    return render(request, "index.html")
 
 
-class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = Menu.objects.all()
-    serializer_class = MenuItemSerializer
+def about(request):
+    return render(request, "about.html")
 
 
-class BookingViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
-    queryset = Booking.objects.all()
-    serializer_class = BookingSerializer
+@csrf_exempt
+def bookings(request):
+    if request.method == "POST":
+        data = json.load(request)
+        exist = Booking.objects.filter(
+            reservation_date=data["reservation_date"]
+        ).filter()
+        if exist:
+            return HttpResponse(
+                "{'error':1}", content_type="application/json"
+            )
+        Booking.objects.create(
+            first_name=data["first_name"],
+            reservation_date=data["reservation_date"],
+            reservation_slot=data["reservation_slot"],
+        )
+        return redirect(reverse("bookings"))
+
+    date = request.GET.get("date", datetime.today().date())
+    bookings = Booking.objects.all().filter(
+        reservation_date=date
+    )
+    booking_json = serializers.serialize("json", bookings)
+    return render(
+        request, "bookings.html", {"bookings": booking_json}
+    )
+
+
+def book(request):
+    form = BookingForm()
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            print("=== valid form===")
+            form.save()
+    context = {"form": form}
+    return render(request, "book.html", context)
+
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, "menu.html", {"menu": main_data})
+
+
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""
+    return render(
+        request, "menu_item.html", {"menu_item": menu_item}
+    )
