f7baa90bd5c6970896f8c978beba15b77bf96f23
zed-chi <zed-chi@mail.ru>
2023-05-07 14:56:15 +0300

    upd

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "LittleLemon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "__templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = ""
+STATICFILES_DIRS = "__static"
+MEDIA_URL = BASE_DIR / "static/"
+MEDIA_ROOT = BASE_DIR / "__media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
